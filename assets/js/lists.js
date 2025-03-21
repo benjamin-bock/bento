239f36958731bcc001bc538ef76b2813191418e6
Benjamin Bock <benjaminbock05@gmail.com>
2025-03-21 17:50:34 +0100

    fix : date 2?

--- assets/js/lists.js
+++ assets/js/lists.js
@@ -3,61 +3,82 @@
 // ┴─┘┴└─┘ ┴ └─┘
 // Functions for printing both lists
 
-document.addEventListener('DOMContentLoaded', () => {
-	const lists_1 = document.getElementById('lists_1');
-	const lists_2 = document.getElementById('lists_2');
+const generateFirstListsContainer = () => {
+	for (const list of CONFIG.firstlistsContainer) {
+		let item = `
+        <div class="card list list__${list.id}" id="list_${list.id}">
+          <i class="listIcon" icon-name="${list.icon}"></i>
+          <a
+          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          href="${list.links[0].link}"
+          class="listItem"
+          >${list.links[0].name}</a>
+          <a
+          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          href="${list.links[1].link}"
+          class="listItem"
+          >${list.links[1].name}</a>
+          <a
+          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          href="${list.links[2].link}"
+          class="listItem"
+          >${list.links[2].name}</a>
+          <a
+          target="${CONFIG.openInNewTab ? '_blank' : ''}"
+          href="${list.links[3].link}"
+          class="listItem"
+          >${list.links[3].name}</a>
+        </div>
+      `;
+		const position = 'beforeend';
+		lists_1.insertAdjacentHTML(position, item);
+	}
+};
 
-	const generateFirstListsContainer = () => {
-		if (!lists_1) return;
-		
-		for (const list of CONFIG.firstlistsContainer) {
-			let iconHtml = list.icon.endsWith('.svg') 
-				? `<img src="${list.icon}" alt="Icon" class="listIcon">` 
-				: `<i class="listIcon" icon-name="${list.icon}"></i>`;
-				
-			let item = `
-				<div class="card list list__${list.id}" id="list_${list.id}">
-					${iconHtml}
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[0].link}" class="listItem">${list.links[0].name}</a>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[1].link}" class="listItem">${list.links[1].name}</a>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[2].link}" class="listItem">${list.links[2].name}</a>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[3].link}" class="listItem">${list.links[3].name}</a>
-				</div>
-			`;
-			lists_1.insertAdjacentHTML('beforeend', item);
-		}
-	};
+const generateSecondListsContainer = () => {
+	for (const list of CONFIG.secondListsContainer) {
+		let item = `
+        <div class="card list list__${list.id}" id="list_${list.id}">
+        <i class="listIcon" icon-name="${list.icon}"></i>
+        <a
+        target="${CONFIG.openInNewTab ? '_blank' : ''}"
+        href="${list.links[0].link}"
+        class="listItem"
+        >${list.links[0].name}</a>
+        <a
+        target="${CONFIG.openInNewTab ? '_blank' : ''}"
+        href="${list.links[1].link}"
+        class="listItem"
+        >${list.links[1].name}</a>
+        <a
+        target="${CONFIG.openInNewTab ? '_blank' : ''}"
+        href="${list.links[2].link}"
+        class="listItem"
+        >${list.links[2].name}</a>
+        <a
+        target="${CONFIG.openInNewTab ? '_blank' : ''}"
+        href="${list.links[3].link}"
+        class="listItem"
+        >${list.links[3].name}</a>
+        </div>
+      `;
+		const position = 'beforeend';
+		lists_2.insertAdjacentHTML(position, item);
+	}
+};
 
-	const generateSecondListsContainer = () => {
-		if (!lists_2) return;
-		
-		for (const list of CONFIG.secondListsContainer) {
-			let item = `
-				<div class="card list list__${list.id}" id="list_${list.id}">
-					<i class="listIcon" icon-name="${list.icon}"></i>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[0].link}" class="listItem">${list.links[0].name}</a>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[1].link}" class="listItem">${list.links[1].name}</a>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[2].link}" class="listItem">${list.links[2].name}</a>
-					<a target="${CONFIG.openInNewTab ? '_blank' : ''}" href="${list.links[3].link}" class="listItem">${list.links[3].name}</a>
-				</div>
-			`;
-			lists_2.insertAdjacentHTML('beforeend', item);
-		}
-	};
+const generateLists = () => {
+	switch (CONFIG.bentoLayout) {
+		case 'bento':
+			generateFirstListsContainer();
+			break;
+		case 'lists':
+			generateFirstListsContainer();
+			generateSecondListsContainer();
+			break;
+		default:
+			break;
+	}
+};
 
-	const generateLists = () => {
-		switch (CONFIG.bentoLayout) {
-			case 'bento':
-				generateFirstListsContainer();
-				break;
-			case 'lists':
-				generateFirstListsContainer();
-				generateSecondListsContainer();
-				break;
-			default:
-				break;
-		}
-	};
-
-	generateLists();
-});
+generateLists();
